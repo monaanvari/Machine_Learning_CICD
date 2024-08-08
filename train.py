e36dc718dbaea9ebb916d94c2ba4854bb36c8fe8
monaanvari <38119560+monaanvari@users.noreply.github.com>
2024-08-07 22:43:50 -0700

    Update train.py

--- train.py
+++ train.py
@@ -60,7 +60,7 @@ f1 = f1_score(y_test, predictions, average="macro")
 
 print("Accuracy:", str(round(accuracy, 2) * 100) + "%", "F1:", round(f1, 2))
 with open("Results/metrics.txt", "w") as outfile:
-    outfile.write(f"\nAccuracy = {accuracy.round(2)}, F1 Score = {f1.round(2)}.")
+    outfile.write(f"\nAccuracy = {accuracy:.2f}, F1 Score = {f1:.2f)}.")
 
 
 '''
